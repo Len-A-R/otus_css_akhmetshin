cfd9cc4839a559c1ad92a9dce8ce420a6b680a2c
len-a-r <len-a-r@mail.ru>
2024-12-14 20:25:24 +0300

    popup

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    const body = document.getElementsByClassName('page');
+    const contactDialog = document.querySelector('#modal-1');
+    const btnOpenContactDialog = document.getElementById('modal-contact');
+    const btnCloseContactDialog = document.getElementById('modal-1-close');
+    
+    const handleModalSwitch = () => {
+        contactDialog.classList.toggle('modal--closed');
+        body.classList.toggle('page--scroll-locked');
+    }
+    
+    if (btnCloseContactDialog) {
+        btnOpenContactDialog.addEventListener('click', handleModalSwitch);
+    }
+    if (btnCloseContactDialog) {
+        btnCloseContactDialog.addEventListener('click', handleModalSwitch);
+    }
+});
