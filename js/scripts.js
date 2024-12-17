88d9c2e3ef275a9c3c60e937c916a8b819805c04
len-a-r <len-a-r@mail.ru>
2024-12-17 11:18:04 +0300

    блокируем скролл при показе модального окна

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    const body = document.getElementsByClassName('page');
+    const body = document.body;
     const contactDialog = document.querySelector('#modal-1');
     const btnOpenContactDialog = document.getElementById('modal-contact');
     const btnCloseContactDialog = document.getElementById('modal-1-close');
     
     const handleModalSwitch = () => {
-        contactDialog.classList.toggle('modal--closed');
-        body.classList.toggle('page--scroll-locked');
+        contactDialog.classList.toggle("modal--closed");
+        body.classList.toggle("page--scroll-locked");
     }
     
     if (btnCloseContactDialog) {
