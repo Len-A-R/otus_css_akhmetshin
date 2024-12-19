6db1b32cc0e8d10fe8dee430627b8075e04cdca2
len-a-r <len-a-r@mail.ru>
2024-12-19 15:48:37 +0300

    разметка форм + декоративные элементы + стилизация элементов форм + transition + animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,9 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-    const body = document.body;
-    const contactDialog = document.querySelector('#modal-1');
-    const btnOpenContactDialog = document.getElementById('modal-contact');
-    const btnCloseContactDialog = document.getElementById('modal-1-close');
-    
-    const handleModalSwitch = () => {
-        contactDialog.classList.toggle("modal--closed");
-        body.classList.toggle("page--scroll-locked");
-    }
-    
-    if (btnCloseContactDialog) {
-        btnOpenContactDialog.addEventListener('click', handleModalSwitch);
-    }
-    if (btnCloseContactDialog) {
-        btnCloseContactDialog.addEventListener('click', handleModalSwitch);
-    }
-});
+function openModal() {
+    document.getElementById('contactFormModal').classList.add('active');
+    document.body.classList.add('modal-open');
+}
+
+function closeModal() {
+    document.getElementById('contactFormModal').classList.remove('active');
+    document.body.classList.remove('modal-open');
+}
